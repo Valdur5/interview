eec871617c05001f5e6bfc40c3225831b563048c
Valentin Durst <ValentinDurst@gmail.com>
2019-05-20 14:03:45 +0200

    SumInList - Fast solution O(n).

---
+++ src/main/java/de/pandigo/sumInList/SumInList.java
@@ -0,0 +1,13 @@
+package de.pandigo.sumInList;
+
+import java.util.List;
+
+public class SumInList {
+
+    private static FastImplementation fastImplementation = new FastImplementation();
+
+    public static void main(String[] args) {
+        List<Pair> pairList = fastImplementation.contains(new int[]{1, 4, 5, 11, -2}, 9);
+        pairList.forEach(p -> System.out.println(p.getNumber1() + " "+ p.getNumber2()));
+    }
+}
